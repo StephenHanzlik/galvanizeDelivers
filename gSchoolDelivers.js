eea08a43df34b37c4ab5861579f30aa902a2e332
Stephen Hanzlik <stephenhanzlik@Stephens-MacBook-Pro.local>
2016-10-23 12:43:22 -0600

    got jQuery click and subtotal to function

--- gSchoolDelivers.js
+++ gSchoolDelivers.js
@@ -20,11 +20,16 @@ function priceConvert(rawPrice) {
 
 var buyBurger = $('#buyBurger');
 var priceBurger = $('#priceBurger').text();
+var subTotalText = $('#subTotal').text();
 
 buyBurger.on('click', function() {
-    var itemPriceStr = priceConvert(priceBurger);
-    var subtotal =
-
+  var itemPrice = parseFloat(priceConvert(priceBurger));
+  var cartSubTotal = parseFloat(priceConvert(subTotalText));
+  var appendTotal = itemPrice + cartSubTotal;
+  //line does not append as expected
+  $('#subTotal').empty();
+  $('#subTotal').append((("$"+ appendTotal)));
+  // $('#subTotal').append($('#priceBurger'));
 });
 
 
