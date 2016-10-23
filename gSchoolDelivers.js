53b318c8bc9db8cb814197064ba6f620f1de4279
Stephen Hanzlik <stephenhanzlik@Stephens-MacBook-Pro.local>
2016-10-22 23:46:59 -0600

    made imporvment to qQuery processing and extractiong of price

--- gSchoolDelivers.js
+++ gSchoolDelivers.js
@@ -1,16 +1,25 @@
-$(document).ready(function(){
-     $('.parallax').parallax();
+"use strict";
+$(document).ready(function() {
+  $('.parallax').parallax();
+  //
+  // var cartAdd = function() {
+  //     $(this)[0].replaceWith(".")
+  // };
+  //
+  $('#burgerBuy').on('click', function() {
+  priceConvert($("#priceBurger"));
 
-     var cartAdd = function () {
-       $(this)[0].replaceWith(".")
-     }
-
-     $('#burgerBuy').on('click', function () {
-        $(this)[0].replaceWith("#price") 
-     }
+  }
 
 
-
+function priceConvert() {
+    var subtotalArr = [];
+    for (var i = 1; i < rawPrice.length; i++) {
+        var digit = rawPrice.charAt(i);
+          subtotalArr.push(digit);
+    }
+    return subtotalArr.join('');
+};
 
 
 });
