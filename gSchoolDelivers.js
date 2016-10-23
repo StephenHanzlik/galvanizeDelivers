14dfbb34eda2c8782bfb3d1c19e7abf322b44eb1
Stephen Hanzlik <stephenhanzlik@Stephens-MacBook-Pro.local>
2016-10-22 23:15:33 -0600

    added images and prices to cards

--- gSchoolDelivers.js
+++ gSchoolDelivers.js
@@ -2,11 +2,12 @@ $(document).ready(function(){
      $('.parallax').parallax();
 
      var cartAdd = function () {
-       $(this)[0].
+       $(this)[0].replaceWith(".")
      }
 
-     $('#burgerBuy').on('click',
-
+     $('#burgerBuy').on('click', function () {
+        $(this)[0].replaceWith("#price") 
+     }
 
 
 
